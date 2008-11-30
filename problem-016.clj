0bc96d1090b43ce1e715c99db1563cbf82f89e1d
david hodges <hodges.david@gmail.com>
2008-11-30 17:12:40 +1100

    added problem 16

---
+++ problem-016.clj
@@ -0,0 +1,34 @@
+#^:shebang '[
+             exec clj clojure.lang.Script "$0" -- "$@"
+             ]
+
+(ns user)
+
+;; http://projecteuler.net/index.php?section=problems&id=16
+;;
+;; Problem 16
+;; 03 May 2002
+;;
+;; 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+;;
+;; What is the sum of the digits of the number 2^1000?
+
+
+(defn pow
+  [n exp]
+  (cond (= n 0)
+        1
+        (= n 1)
+        n
+        :else
+        (let [base (ref n)]
+          (dotimes [i (dec exp)]
+            (dosync (ref-set base (* @base n))))
+          @base)))
+
+
+(defn solution
+  []
+  (reduce + (map #(- (int %) 48) 
+                 (seq (str (pow 2 1000))))))
+
