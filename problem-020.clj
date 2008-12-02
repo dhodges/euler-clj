2aec5c4e9086e2860ea6c3414acde8f104f538d3
david hodges <hodges.david@gmail.com>
2008-12-02 19:57:10 +1100

    Solved. Answer: 648

---
+++ problem-020.clj
@@ -0,0 +1,32 @@
+#^:shebang '[
+             exec clj clojure.lang.Script "$0" -- "$@"
+             ]
+
+(ns user)
+
+;; http://projecteuler.net/index.php?section=problems&id=20
+;;
+;; Problem 20
+;; 21 June 2002
+;;
+;; n! means n × (n − 1) × ... × 3 × 2 × 1
+;;
+;; Find the sum of the digits in the number 100!
+
+(defn fib
+  [n]
+  (if (= n 1)
+    1
+    (* n (fib (dec n)))))
+
+        
+(defn solution
+  []
+  (time
+   (apply + 
+          (map #(- (int %) 48)
+               (str (fib 100))))))
+
+(defn test
+  []
+  (= (solution) 648))
