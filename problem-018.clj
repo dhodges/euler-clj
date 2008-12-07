d6db4af38399a573ddfd6c223a7331b8fffd99a0
david hodges <hodges.david@gmail.com>
2008-12-07 12:58:24 +1100

    trying a solution whereby each row is scanned for a subset of highest values

--- problem-018.clj
+++ problem-018.clj
@@ -38,21 +38,21 @@
 
 (defstruct node :val :left :right)
 
-(def number-rows '((75)
-                   (95  64)
-                   (17  47  82)
-                   (18  35  87  10)
-                   (20   4  82  47  65)
+(def number-rows           '((75)
+                           (95  64)
+                         (17  47  82)
+                       (18  35  87  10)
+                     (20   4  82  47  65)
                    (19   1  23  75   3  34)
-                   (88   2  77  73   7  63  67)
-                   (99  65   4  28   6  16  70  92)
-                   (41  41  26  56  83  40  80  70  33)
-                   (41  48  72  33  47  32  37  16  94  29)
-                   (53  71  44  65  25  43  91  52  97  51  14)
-                   (70  11  33  28  77  73  17  78  39  68  17  57)
-                   (91  71  52  38  17  14  91  43  58  50  27  29  48)
-                   (63  66   4  68  89  53  67  30  73  16  69  87  40  31)
-                   ( 4  62  98  27  23   9  70  98  73  93  38  53  60   4  23)))
+                 (88   2  77  73   7  63  67)
+               (99  65   4  28   6  16  70  92)
+             (41  41  26  56  83  40  80  70  33)
+           (41  48  72  33  47  32  37  16  94  29)
+         (53  71  44  65  25  43  91  52  97  51  14)
+       (70  11  33  28  77  73  17  78  39  68  17  57)
+     (91  71  52  38  17  14  91  43  58  50  27  29  48)
+   (63  66   4  68  89  53  67  30  73  16  69  87  40  31)
+ ( 4  62  98  27  23   9  70  98  73  93  38  53  60   4  23)))
 
 
 (defn make-tree
@@ -98,7 +98,57 @@
         (recur @new-totals (rest rows))))))
 
          
-(find-max-total number-rows)
+;(find-max-total number-rows)
 
 ; != 849
+; != 1064
+
+; Something to try:
+;
+; Starting from the bottom row choose a subset, e.g. the top five numbers,
+; and for each number chosen work back to the top of the tree,
+; choosing the max of two choices with each step.
+; e.g:
+; 75
+; 95
+; 47
+; 87
+; 82
+; 75
+; 73
+; 28
+; 83
+; 47
+; 43
+; 73
+; 91
+; 67
+; 98
+
+; Something else to try:
+;
+; Scan each line from top to bottom.
+; Note the top 5 numbers on each line.
+; For each saved line of 5 numbers, try to connect them up to as many paths as possible.
+; Return the maximum total from all the successful paths.
+
+(defstruct nub :value :index)
+
+(def nub-comp (proxy [java.util.Comparator] []
+                (compare [o1 o2] 
+                         (let [v1 (o1 :value) v2 (o2 :value)]
+                           (cond (< v1 v2) -1
+                                 (= v1 v2)  0
+                                 :else      1)))))
+(defn max-in-each-row
+  [n rows]
+  (for [r rows]
+    (take n 
+          (reverse 
+           (sort nub-comp 
+                 (for [ndx (range (count r))]
+                   (struct nub (nth r ndx) ndx)))))))
+         
+        
+                 
 
