a0240ae9f464a1e8ca7c866fa9221d64b62409cf
david hodges <hodges.david@gmail.com>
2008-12-12 17:08:37 +1100

    Three unsolved problems

---
+++ problem-039.clj
@@ -0,0 +1,19 @@
+#^:shebang '[
+             exec clj clojure.lang.Script "$0" -- "$@"
+             ]
+
+(ns user)
+
+;; http://projecteuler.net/index.php?section=problems&id=39
+;;
+;; Problem 39
+;; 14 March 2003
+;;
+;; If p is the perimeter of a right angle triangle with
+;; integral length sides, {a,b,c}, there are exactly three solutions
+;; for p = 120.
+;;
+;; {20,48,52}, {24,45,51}, {30,40,50}
+;;
+;; For which value of p ≤ 1000, is the number of solutions maximised?
+
