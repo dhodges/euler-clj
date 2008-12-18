5e68fdf460b7fa89546febfe052dfdaf055185a6
david hodges <hodges.david@gmail.com>
2008-12-18 23:50:30 +1100

    Solution: 932718654

--- problem-038.clj
+++ problem-038.clj
@@ -2,7 +2,10 @@
              exec clj clojure.lang.Script "$0" -- "$@"
              ]
 
-(ns user)
+(ns dh.euler
+  (:use [project_euler.dh_utils])
+  (:use [clojure.contrib.trace])
+  (:use [clojure.contrib.test-is]))
 
 ;; http://projecteuler.net/index.php?section=problems&id=38
 ;;
@@ -25,3 +28,51 @@
 ;; What is the largest 1 to 9 pandigital 9-digit number that can be
 ;; formed as the concatenated product of an integer with (1,2, ... , n)
 ;; where n > 1?
+
+
+(defn catprod-pandigital
+  [number]
+  (loop [n 1
+         catprod (str number)]
+    (cond (not (= catprod (apply str (distinct catprod))))
+          false
+          (and (= (count catprod) 9)
+               (pandigital? catprod))
+          [number (Long/parseLong catprod)]
+          :else
+          (recur (inc n)
+                 (strcat catprod (* number (inc n)))))))
+
+(def cmp (proxy [java.util.Comparator] []
+           (compare [o1 o2]
+                    (let [num1 (second o1)
+                          num2 (second o2)]
+                      (cond (< num1 num2)
+                            -1
+                            (= num1 num2)
+                            0
+                            :else
+                            1)))))
+
+(defn generate-pandigitals
+  [limit]
+  (loop [n 1
+         pans []]
+    (if (> n limit)
+      (reverse (sort cmp pans))
+      (let [pan (catprod-pandigital n)]
+        (recur (inc n)
+               (if pan
+                 (conj pans pan)
+                 pans))))))
+
+(defn euler-38
+  []
+  (time
+   (second (first (generate-pandigitals 10000)))))
+
+
+(deftest test-euler-38
+  (is (= (euler-38) 932718654)))
+
+
