fd08488f396148e5c4d95ba92a9932e9fcdf3b3e
david hodges <hodges.david@gmail.com>
2008-12-20 22:38:18 +1100

    started porting integer-partition algorithm from python

--- problem-031.clj
+++ problem-031.clj
@@ -23,3 +23,40 @@
 
 (def coins '(100 50 20 5 2 1))
 
+(defn set-nth
+  "set the nth item of sequence, counted 0...n-1"
+  [sequ ndx item]
+  (concat (take (dec ndx) sequ) [1] (drop ndx sequ)))
+
+(defn generate-partitions
+  "incomplete"
+  [n]
+  (let [x (ref (cons n (for [x (range (dec n))] 1)))
+        m (ref 1)
+        h (ref 1)]
+    (println (take 1 @x))
+    (dosync
+     (while (not (= (first @x) 1))
+       (if (= (nth @x (dec @h)) 2)
+         (do
+           (ref-set m (inc @m))
+           (ref-set x (set-nth @x (dec @h) 1))
+           (ref-set h (- @h 1)))
+         (do
+           (let [r (- (nth @x (- @h 1)) 1)
+                 t (ref (inc (- @m @h)))]
+             (ref-set x (set-nth @x (dec @h) r))
+             (while (>= @t r)
+               (ref-set h (inc @h))
+               (ref-set x (set-nth @x (dec @h) r))
+               (ref-set t (- @t @r)))
+
+             (if (= @t 0)
+               (ref-set m @h)
+               (do
+                 (ref-set m (inc @h))
+                 (if (> @t 1)
+                   (ref-set h (inc @h))
+                   (ref-set x (set-nth @x (dec @h) @t))))))))
+       (println @x)))))
+
