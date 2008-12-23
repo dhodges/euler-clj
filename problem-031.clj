a927839e4c39b510ed291177fd99a6bbf0a9cc9c
david hodges <hodges.david@gmail.com>
2008-12-24 08:03:57 +1100

    added solution from SICP

--- problem-031.clj
+++ problem-031.clj
@@ -98,7 +98,7 @@
            (let [least (min-above-1 partition)
                  ndx   (rindex-of partition least)
                  least (decfn least)
-                 part  (into (into [] (take ndx partition)) [least])
+                 part  (conj (into [] (take ndx partition)) least)
                  sum   (apply + part)
                  part  (into part (for [x (range (quot (- n sum) least))] least))
                  sum   (apply + part)
@@ -111,6 +111,38 @@
   [n]
   (count-partitions n dec-coins))
 
+
+
+
+; from SICP, p. 40
+; Example: Counting change
+
+(defn first-denomination
+  [kinds-of-coins]
+  (cond (= kinds-of-coins 1) 1
+        (= kinds-of-coins 2) 2
+        (= kinds-of-coins 3) 5
+        (= kinds-of-coins 4) 10
+        (= kinds-of-coins 5) 20
+        (= kinds-of-coins 6) 50
+        (= kinds-of-coins 7) 100
+        (= kinds-of-coins 8) 200))
+
+
+(defn cc
+  [amount kinds-of-coins]
+  (cond (= amount 0)
+        1
+        (or (< amount 0) (= kinds-of-coins 0))
+        0
+        :else
+        (+ (cc amount
+               (- kinds-of-coins 1))
+           (cc (- amount
+                  (first-denomination kinds-of-coins))
+               kinds-of-coins))))
+
+(time (cc 200 8))
 
 
 
