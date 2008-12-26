63a79a8bd510684ef34fc45efabae9ad86bebb44
david hodges <hodges.david@gmail.com>
2008-12-26 11:33:19 +1100

    first few ideas

--- problem-039.clj
+++ problem-039.clj
@@ -2,7 +2,10 @@
              exec clj clojure.lang.Script "$0" -- "$@"
              ]
 
-(ns dh.euler)
+(ns dh.euler
+  (:use [clojure.contrib.test-is])
+  (:use [clojure.contrib.seq-utils])
+  (:use [project_euler.dh_utils]))
 
 ;; http://projecteuler.net/index.php?section=problems&id=39
 ;;
@@ -16,4 +19,36 @@
 ;; {20,48,52}, {24,45,51}, {30,40,50}
 ;;
 ;; For which value of p ≤ 1000, is the number of solutions maximised?
+
+(defn integral-square?
+  "is 'n' the square of an integral?"
+  [n]
+  (let [sqint (int (Math/sqrt n))]
+    (= n (* sqint sqint))))
+
+
+(defn triangle-partition?
+  [triple]
+  (let [[a b c] triple]
+    (= (+ (* a a) (* b b))
+       (* c c))))
+
+
+(defn triangle-partitions
+  [n]
+  (distinct
+   (map sort 
+        (apply concat
+               (filter #(not (nil? %))
+                       (for [c (range 5 n)]
+                         (let [diff (- n c)]
+                           (for [a (range 1 diff)]
+                             (let [b (- diff a)]
+                               [a b c])))))))))
+          
+          
+(defn triangle-partitions
+  [n]
+  (filter (triangle-partition?
+           (triangle-partitions n))))
 
