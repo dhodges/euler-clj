53272dd848115ff95efeaa282aa6b92823471c9b
david hodges <hodges.david@gmail.com>
2013-09-10 17:30:12 +1000

    added dependency tools.nrepl

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject dh.euler-clj "0.1.0-SNAPSHOT"
   :description "my solutions for projecteuler.net"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.nrepl "0.2.3"]]
   )
