2857675f5474617e281c4d6e5a84ac0c1b13a5ec
david hodges <hodges.david@gmail.com>
2013-09-13 17:43:38 +1000

    problem_014

--- src/dh/euler/solutions/problem_014.clj
+++ src/dh/euler/solutions/problem_014.clj
@@ -1,51 +1,48 @@
-(ns dh.euler.problems.problem_014)
-
-;; Problem 14
-
-;; http://projecteuler.net/index.php?section=problems&id=14
-
 ;; The following iterative sequence is defined for the set of positive integers:
-
+;;
 ;; n → n/2 (n is even)
 ;; n → 3n + 1 (n is odd)
-
+;;
 ;; Using the rule above and starting with 13, we generate the following sequence:
 ;; 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-
+;;
 ;; It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms.
 ;; Although it has not been proved yet (Collatz Problem), it is thought that
 ;; all starting numbers finish at 1.
-
+;;
 ;; Which starting number, under one million, produces the longest chain?
-
+;;
 ;; NOTE: Once the chain starts the terms are allowed to go above one million.
+;;
+;; http://projecteuler.net/problem=14
+;;
+;; Answer: 837799
 
-(defstruct term :start :chain :len)
+(ns dh.euler.solutions.problem_014)
 
-(defn gen-chain
+
+(defn collatz-chain
   [n]
   (if (= n 1)
     '(1)
-    (cons n (gen-chain (if (even? n) (/ n 2) (inc (* 3 n)))))))
+    (cons n (collatz-chain (if (even? n)
+                             (/ n 2)
+                             (inc (* 3 n)))))))
 
+(defstruct chain :n :len)
 
 (defn euler-014
   []
-  (loop [n 1
-         t (struct-map term
-             :start 13
-             :chain '(13 40 20 10 5 16 8 4 2 1)
-             :len 10)]
+  (loop [n 0
+         max-chain (struct-map chain :n 13 :len 10)]
     (if (>= n 1000000)
-      t
-      (let [chain (gen-chain n)]
-        (recur (inc n)
-               (if (> (count chain) (t :len))
-                 (struct-map term :start n :chain chain :len (count chain))
-                 t))))))
-
-(deftest test-euler-014
-  (is (= (solution) 837799)))
-
-
-
+      (max-chain :n)
+      (do
+        (let [n (inc n)
+              new-chain (struct-map chain
+                          :n n
+                          :len (count(collatz-chain n)))]
+          (if (> (new-chain :len)
+                 (max-chain :len))
+            (recur n new-chain)
+            (recur n max-chain)))))))
---
+++ test/dh/euler/solutions/problem_014_test.clj
@@ -0,0 +1,7 @@
+(ns dh.euler.solutions.problem_014_test
+  (:require [dh.euler.solutions.problem_014 :refer :all]
+            [clojure.test :refer :all]))
+
+
+(deftest test-euler-014
+  (is (= (euler-014) 837799)))
