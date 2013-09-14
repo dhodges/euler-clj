3aa5a682e4fc56c6ec4e3aa09cb170b91ebc59b4
david hodges <hodges.david@gmail.com>
2013-09-14 20:33:29 +1000

    moved data files to resources dir

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "my solutions for projecteuler.net"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.nrepl "0.2.3"]]
+  :resource-paths ["resources"]
   )
