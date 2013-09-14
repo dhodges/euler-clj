c39685aa0da2d8eead0f1f5e30de4bc51c0e2989
david hodges <hodges.david@gmail.com>
2013-09-14 10:52:32 +1000

    problem_020

---
+++ src/dh/euler/solutions/problem_020.clj
@@ -0,0 +1,16 @@
+;; n! means n × (n − 1) × ... × 3 × 2 × 1
+;;
+;; Find the sum of the digits in the number 100!
+;;
+;; http://projecteuler.net/problem=20
+;;
+;; Answer: 648
+
+(ns dh.euler.solutions.problem_020
+  (:use [dh.euler.util.core :refer [factorial]]))
+
+(defn euler-020
+  []
+  (apply +'
+         (map #(- (int %) 48)
+              (str (factorial 100)))))
---
+++ test/dh/euler/solutions/problem_020_test.clj
@@ -0,0 +1,7 @@
+(ns dh.euler.solutions.problem_020_test
+  (:require [dh.euler.solutions.problem_020 :refer :all]
+            [clojure.test :refer :all]))
+
+
+(deftest test-euler-020
+  (is (= (euler-020) 648)))
