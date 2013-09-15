fbc1f5775a2f7ad5789e0726c59c6739f3efaa2f
david hodges <hodges.david@gmail.com>
2013-09-15 13:53:12 +1000

    problem_028

--- src/dh/euler/solutions/problem_028.clj
+++ src/dh/euler/solutions/problem_028.clj
@@ -1,10 +1,3 @@
-(ns dh.euler.problems.problem_028)
-
-;; http://projecteuler.net/index.php?section=problems&id=28
-;;
-;; Problem 28
-;; 11 October 2002
-;;
 ;; Starting with the number 1 and moving to the right in a clockwise direction
 ;; a 5 by 5 spiral is formed as follows:
 ;;
@@ -16,7 +9,13 @@
 ;;
 ;; It can be verified that the sum of both diagonals is 101.
 ;;
-;; What is the sum of both diagonals in a 1001 by 1001 spiral formed in the same way?
+;; What is the sum of both diagonals in a 1001 by 1001 spiral
+;; formed in the same way?
+;;
+;; http://projecteuler.net/problem=28
+
+(ns dh.euler.solutions.problem_028)
+
 
 ;
 ; Notes:
@@ -46,17 +45,9 @@
 (defn south-east [n] (+ 3 (* n (- n 3))))
 
 (defn euler-028
-  [n]
-  (time
-   (reduce + 1 (for [x (range n 1 -2)]
-                 (+ (north-east x)
-                    (north-west x)
-                    (south-west x)
-                    (south-east x))))))
-
-
-(deftest test-euler-028
   []
-  (is (= (solution 1001) 669171001)))
-
-
+  (reduce + 1 (for [x (range 1001 1 -2)]
+                (+ (north-east x)
+                   (north-west x)
+                   (south-west x)
+                   (south-east x)))))
