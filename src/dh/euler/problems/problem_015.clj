92fc5d8cb8ee2040879d9298de8b6828f9fb20ba
david hodges <hodges.david@gmail.com>
2013-09-15 21:01:37 +1000

    tweak problem 15

--- src/dh/euler/problems/problem_015.clj
+++ src/dh/euler/problems/problem_015.clj
@@ -1,14 +1,12 @@
-(ns dh.euler.problems.problem_015
-  (:use [dh.euler.utils]))
-
-;; Problem 15
-;;
-;; http://projecteuler.net/index.php?section=problems&id=15
-;;
 ;; Starting in the top left corner of a 2 x 2 grid, there are 6 routes
 ;; (without backtracking) to the bottom right corner.
 ;;
 ;; How many routes are there through a 20 x 20 grid?
+;;
+;; http://projecteuler.net/problem=15
+
+(ns dh.euler.problems.problem_015
+  (:use [dh.euler.util.core :refer [pow]]))
 
 
 (defn grid-routes
@@ -26,5 +24,44 @@
    (grid-routes 20)))
 
 
-(deftest test-euler-015
-  (is (= (euler-015) 137846528820)))
+;; (deftest test-euler-015
+;;   (is (= (euler-015) 137846528820)))
+
+(defn gridseq-n
+  [n]
+  (if (= n 0)
+    0
+    (+' (*' 2 n)
+        (gridseq-n (dec n)))))
+
+(defn path-count
+  [n]
+  (if (= n 1)
+    2
+    (+ (pow 2 n)
+       (path-count (dec n)))))
+
+
+;; . . . . . .
+;; . . . . . .
+;; . . . . . .
+;; . . . . . .
+;; . . . . . .
+;; . . . . . .
+
+;; 0 x 0 =  0
+;; 1 x 1 =  2
+;; 2 x 2 =  6
+;; 3 x 3 = 14
+;; 4 x 4 = 30
+
+;; . . . .
+;; . . . .
+;; . . . .
+;; . . . .
+
+;; . . . . .
+;; . . . . .
+;; . . . . .
+;; . . . . .
+;; . . . . .
