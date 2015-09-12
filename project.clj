d3cd5041992b8aeaa5013cc4e9c84c02d5852069
david hodges <david.hodges@lonelyplanet.com>
2015-09-12 18:12:03 +1000

    bump clojure version to 1.7.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dh.euler-clj "0.1.0-SNAPSHOT"
   :description "my solutions for projecteuler.net"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/math.combinatorics "0.1.1"]
                  [org.clojure/math.numeric-tower "0.0.2"]
                  [org.clojure/tools.nrepl "0.2.7"]
