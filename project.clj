8ebf9c55e5cf6eb06bbb318162e4a708eef5e881
david hodges <david.hodges@lonelyplanet.com>
2015-09-13 19:23:11 +1000

    update project dependencies

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "my solutions for projecteuler.net"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/math.combinatorics "0.1.1"]
-                 [org.clojure/math.numeric-tower "0.0.2"]
-                 [org.clojure/tools.nrepl "0.2.7"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/tools.nrepl "0.2.9"]
                  ]
   :resource-paths ["resources"]
   )
